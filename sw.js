d3ddcebbf0ddc867b474c64fc7766a9db932e34c
bang710-dotcom <bang710@gmail.com>
2026-06-15 17:46:18 +0900

    Auto: total-sermon v104 2026-06-15 17:46

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 토탈 설교 앱 — 오프라인 셸 캐시 */
-var CACHE = 'tsa-v96';
+var CACHE = 'tsa-v97';
 var ASSETS = ['./index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
