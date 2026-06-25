d92fcaa7c7039360e98ec0e1e036264ce088b21c
Bang Deokjong <bang710@gmail.com>
2026-06-25 23:44:05 +0900

    Auto: total-sermon v290 2026-06-25 23:44

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 토탈 설교 앱 — 오프라인 셸 캐시 */
-var CACHE = 'tsa-v289';
+var CACHE = 'tsa-v290';
 var ASSETS = ['./index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './illustrations.json'];
 
 self.addEventListener('install', function (e) {
